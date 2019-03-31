249f94d621d8bbda6d08be32e43bba06d076d6c9
Cristian <cristianrojas_lara@hotmail.com>
2019-03-30 18:34:48 -0600

    commitv48
    creasion del proyecto asi como los cambios de configuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/shared/navbar/navbar.component';
+import { HomeComponent } from './componets/home/home.component';
+import { HeroesComponent } from './componets/heroes/heroes.component';
+import { AboutComponent } from './componets/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
